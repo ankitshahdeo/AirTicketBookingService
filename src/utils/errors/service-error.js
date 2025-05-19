dfa2cadecb90b494e4b241c3810bf821572cb482
ankitshahdeo <ankitshahdeo29@gmail.com>
2025-05-19 15:30:04 +0530

    completed create booking

--- src/utils/errors/service-error.js
+++ src/utils/errors/service-error.js
@@ -1,16 +1,16 @@
-const { StatusCodes} = require('http-status-codes');
+const { StatusCodes } = require('http-status-codes');
 
 class  ServiceError extends Error {
     constructor(
         message= 'Something went wrong',
         explanation ='Service layer error', 
-        statusCodes= StatusCodes.INTERNAL_SERVER_ERROR
+        statusCode= StatusCodes.INTERNAL_SERVER_ERROR
     ){
         super();
         this.name = 'ServiceError';
         this.message = message;
         this.explanation = explanation;
-        this.statusCode = this.statusCode;
+        this.statusCode = statusCode;
     }
 }
 module.exports = ServiceError;
